7527e35c1e67c9b2e2b87ddcd01a001812801a43
Hsu_Tog <1198266201@qq.com>
2022-11-05 23:46:18 +0800

    update UIdemo

---
+++ Assets/Scripts/UI/Button.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour
+{
+    public void loadScene(int sceneID)
+    {
+        SceneManager.LoadScene(sceneID);
+    }
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
